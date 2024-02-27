61a322e56d2d783c2d5260a9fee71b7a30205a2d
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-02-27 18:08:10 -0500

    first activites layouts

---
+++ app/src/main/java/together/devs/playtogether/NewFieldActivity.kt
@@ -0,0 +1,11 @@
+package together.devs.playtogether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewFieldActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_field)
+    }
+}
