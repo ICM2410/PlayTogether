a7c72c5c151797da858d71fbdfdb6ec2cc7abe0d
ceron <73488150+nicoceron@users.noreply.github.com>
2024-04-23 22:15:38 -0500

    logic & firebase

---
+++ app/src/main/java/together/devs/playtogether/Calificacion.kt
@@ -0,0 +1,11 @@
+package together.devs.playtogether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calificacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calificacion)
+    }
+}
